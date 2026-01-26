342ad5015fc8994c09f131e7dcd52e1c206a851a
Amrin <amrinfathimaaf321@gmail.com>
2026-01-26 15:03:06 +0530

    connected db

---
+++ config/database.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const connectDb = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://amrin:mXkqdIT6hITqFIyM@cluster0.yr19snl.mongodb.net/devtinder?retryWrites=true&w=majority"
+    );
+
+    console.log("Database connected successfully");
+  } catch (err) {
+    console.error("Database connection failed:", err.message);
+    process.exit(1);
+  }
+};
+
+connectDb();
+module.exports = connectDb;
--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 const express = require('express');
+// require('../config/database');
+const connectDB = require('../config/database');
 
 const app = express();
 
+app.post('/addUser', async (req, res)=>{
+    try{
+        const user = new User({
+            firstName: 'Amrin',
+            lastName: 'Fathima',
+            age: 24,
+            email: 'amrin@gmail.com'
+        });
+
+        await user.save();
+        res.send('User Added!');
+    }catch(err){
+        res.send('Error adding user' , err.message);
+    }
+});
+
 app.get('/', (req, res) => {
-    res.send('Server is working... 🚀');
-});
+    res.send('Hello world!');
+})
+
+ app.listen(7777, () => {
+ console.log(`Server is successfully running on 7777`);
+ });
 
 
-
-app.listen(7777, () => {
-    console.log('server listening'); 
-});
