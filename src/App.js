436e9f32e00c19bb27b577ee662b1a8f9b906433
Amrin <amrinfathimaaf321@gmail.com>
2026-01-28 21:03:58 +0530

    created update api, installed validator

--- src/App.js
+++ src/App.js
@@ -10,13 +10,45 @@ app.use(express.json());
 
 app.post('/addUser', async (req, res)=>{
     try{
-        const user = new User(req.body);
+        const data = req.body;
+
+        const allowed_field = ['firstName', 'lastName', 'age', 'email', 'gender'];
+
+        const check_allowed_fields = Object.keys(data).every(k => {
+            allowed_field.includes(k);
+        });
+
+        if(!check_allowed_fields) throw new Error("You cannot add extra fields!");
+
+        const user = new User(data);
 
         await user.save();
         res.send('User Added!');
          
     }catch(err){
         res.status(500).send('Error adding user: ' + err.message);
+    }
+});
+
+app.patch('/updateUser/:userId', async (req, res) => {
+    try{
+        const userId = req.params.userId;
+        const data = req.body;
+
+        const allowed_field = ['firstName', 'lastName', 'age', 'email', 'gender'];
+
+        const check_allowed_fields = Object.keys(data).every(k => {
+           return allowed_field.includes(k);
+        });
+
+        if(!check_allowed_fields) throw new Error("You cannot add extra fields!");
+
+        await User.findOneAndUpdate({ _id: userId }, data, { new: true });
+
+        res.send('User Updated!')
+
+    }catch(err){
+        res.send('Update failed!' + err.message)
     }
 });
 
