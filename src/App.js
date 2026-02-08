6150f0696f491a2db78725a2d2b5d61552338790
Amrin <amrinfathimaaf321@gmail.com>
2026-02-08 20:01:45 +0530

    installed cors

--- src/App.js
+++ src/App.js
@@ -6,9 +6,14 @@ const authRouter= require('./routes/Auth');
 const profileRouter= require('./routes/Profile');
 const requestRouter= require('./routes/Request');
 const userRouter= require('./routes/User');
-
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors({
+   origin: "http://localhost:5173",
+   credentials: true
+}));
 
 app.use(express.json());
 app.use(cookieParser());
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -44,7 +44,7 @@ authRouter.post('/login', async (req, res) => {
         const token = await user.getJWT();
 
         res.cookie('token', token);
-        res.send('Login successfull!');
+        res.send(user);
 
     }catch(err){
         res.send('Login Failed!' + err.message)
